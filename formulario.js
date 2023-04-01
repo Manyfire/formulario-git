b5b62de35f524e359c187e61c543283d25b0a52e
ORT <ort@many.com>
2023-04-01 11:55:27 -0300

    Comenze y pare en las expresiones

---
+++ formulario.js
@@ -0,0 +1,7 @@
+const $formulario = document.getElementById('formulario');//$ se utiliza para buscar o rescatar el formulario
+const $inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    usuario : []
+}
+
